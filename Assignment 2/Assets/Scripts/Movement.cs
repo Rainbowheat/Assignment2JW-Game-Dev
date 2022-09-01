387f2185639f335a76654c671610404940d8264e
Rainbowheat <jacksonwignall@icloud.com>
2022-09-01 17:18:05 +1000

    The Game

---
+++ Assignment 2/Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public bool onGround;
+    public Rigidbody myBody;
+    public Collider myCollider;
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        verticalInput = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(horizontalInput, 0, verticalInput);
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            myBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            onGround = false;
+        }
+        
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("WHA");
+        if (other.tag == "Floor")
+        {
+            onGround = true;
+        }
+    }
+}
